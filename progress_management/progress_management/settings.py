6064d071ceb51e47e2620a8b8ed92b9756793d38
s1f102200705 藤島悠太 <s1f102200705@iniad.org>
2025-03-05 17:37:26 +0900

    27

--- progress_management/progress_management/settings.py
+++ progress_management/progress_management/settings.py
@@ -54,7 +54,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+ALLOWED_HOSTS = ['litalicowonder.onrender.com']
 
 CSRF_TRUSTED_ORIGINS = ['https://litalicowonder.onrender.com']
 
