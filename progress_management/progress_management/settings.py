ea48b6bb7068977661456d9290cf01b682836b32
s1f102200705 藤島悠太 <s1f102200705@iniad.org>
2025-03-06 17:35:56 +0900

    102

--- progress_management/progress_management/settings.py
+++ progress_management/progress_management/settings.py
@@ -24,7 +24,7 @@ DEBUG = True
 
 import dj_database_url
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600, ssl_require=True)
 }
 
 BASE_DIR = Path(__file__).resolve().parent.parent
