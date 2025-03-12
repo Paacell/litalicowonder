a628966e7036602e606e31bdca604b8e477a97ae
s1f102200705 藤島悠太 <s1f102200705@iniad.org>
2025-03-12 17:13:59 +0900

    185

--- progress_management/goals/views.py
+++ progress_management/goals/views.py
@@ -9,24 +9,24 @@ def is_admin(user):
 @login_required
 def goal_list(request):
     goal = Goal.objects.first()  # 目標は1つだけ管理
-    return render(request, "goals/student_goal_list.html", {"goal": goal})
+    return render(request, "goals/goal_list.html", {"goal": goal})
 
-@user_passes_test(is_admin)  # 管理者のみ編集可能
-@login_required
-def edit_goal(request):
-    goal = Goal.objects.first()
-    if not goal:
-        goal = Goal.objects.create(title="未設定", description="ここに目標を入力してください。")
+# @user_passes_test(is_admin)  # 管理者のみ編集可能
+# @login_required
+# def edit_goal(request):
+#     goal = Goal.objects.first()
+#     if not goal:
+#         goal = Goal.objects.create(title="未設定", description="ここに目標を入力してください。")
 
-    if request.method == "POST":
-        form = GoalForm(request.POST, instance=goal)
-        if form.is_valid():
-            form.save()
-            return redirect("goals:student_goal_list")
-    else:
-        form = GoalForm(instance=goal)
+#     if request.method == "POST":
+#         form = GoalForm(request.POST, instance=goal)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("goals:student_goal_list")
+#     else:
+#         form = GoalForm(instance=goal)
 
-    return render(request, "goals/edit_goal.html", {"form": form})
+#     return render(request, "goals/edit_goal.html", {"form": form})
 
 
 
