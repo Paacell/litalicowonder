82fbe65715305ced825784ebfc7ce39e7f575af5
s1f102200705 藤島悠太 <s1f102200705@iniad.org>
2025-03-12 14:55:01 +0900

    167

--- progress_management/progress/urls.py
+++ progress_management/progress/urls.py
@@ -17,7 +17,7 @@ from . import views
 app_name = 'progress'
 
 urlpatterns = [
-    path('', views.game_list, name='game_list'),
+    path('', views.game_list, name='progress_list'),
     # path('add/', views.add_progress, name='add_progress'),
     # path('<int:progress_id>/edit/', views.edit_progress, name='edit_progress'),
     # path('<int:progress_id>/delete/', views.delete_progress, name='delete_progress'),
