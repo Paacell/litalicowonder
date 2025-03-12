245b255d9a4e978a853d84f114fb43c7b63cae2c
s1f102200705 藤島悠太 <s1f102200705@iniad.org>
2025-03-12 18:07:40 +0900

    192

--- progress_management/goals/urls.py
+++ progress_management/goals/urls.py
@@ -8,14 +8,14 @@
 #     path("edit/", edit_goal, name="edit_goal"),
 # ]
 
-from .views import goal_list
+from .views import my_goals
 from django.urls import path
 from . import views
 
 app_name = "goals"
 
 urlpatterns = [
-    path("", goal_list, name="goal_list"),
+    path("", my_goals, name="my_goals"),
     path("students/", views.student_list, name="student_list"),  # 生徒一覧
     path("students/<int:student_id>/goals/", views.student_goal_list, name="student_goal_list"),  # 生徒ごとの目標一覧
     path("goal/edit/<int:goal_id>/", views.goal_edit, name="goal_edit"),  # 目標の編集
