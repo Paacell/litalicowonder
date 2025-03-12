d2e2108a4cd7556acc18f5343b83e906b6b11a27
s1f102200705 藤島悠太 <s1f102200705@iniad.org>
2025-03-12 16:23:41 +0900

    180

--- progress_management/goals/models.py
+++ progress_management/goals/models.py
@@ -1,9 +1,22 @@
+# from django.db import models
+
+# class Goal(models.Model):
+#     title = models.CharField(max_length=255)
+#     description = models.TextField()
+#     updated_at = models.DateTimeField(auto_now=True)
+
+#     def __str__(self):
+#         return self.title
+
+
 from django.db import models
+from accounts.models import CustomUser  # 生徒のアカウントを参照するためにインポート
 
 class Goal(models.Model):
+    student = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="goals")  # 生徒との紐付け
     title = models.CharField(max_length=255)
     description = models.TextField()
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
+        return f"{self.student.username} - {self.title}"
